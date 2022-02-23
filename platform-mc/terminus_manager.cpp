c0f66c989dab52cc5a3945f1a6c0c8f26d265e4d
Gilbert Chen <gilbert.chen@arm.com>
2022-02-23 20:56:19 +0000

    platform-mc: Sensor handling
    Added sensor_manager and numeric_sensor class. The sensor_manager
    class manages the timing of sensor polling. The NumericSensor class
    handles sensor status and exports its status to D-Bus interfaces.
    
    tested: Verified on ast2600 EVB which is connected to a PLDM device
    over I2C. bmcweb can display the state of numeric sensor.
    
    Signed-off-by: Gilbert Chen <gilbert.chen@arm.com>
    Signed-off-by: Thu Nguyen <thu@os.amperecomputing.com>
    Change-Id: I4257f823ea26d7fdb322cc82d847e94db056258c

--- platform-mc/terminus_manager.cpp
+++ platform-mc/terminus_manager.cpp
@@ -171,6 +171,7 @@ auto TerminusManager::findTeminusPtr(const MctpInfo& mctpInfo)
 
 exec::task<int> TerminusManager::discoverMctpTerminusTask()
 {
+    std::vector<pldm_tid_t> addedTids;
     while (!queuedMctpInfos.empty())
     {
         if (manager)
@@ -186,11 +187,23 @@ exec::task<int> TerminusManager::discoverMctpTerminusTask()
             {
                 co_await initMctpTerminus(mctpInfo);
             }
+
+            /* Get TID of initialized terminus */
+            auto tid = toTid(mctpInfo);
+            if (!tid)
+            {
+                co_return PLDM_ERROR;
+            }
+            addedTids.push_back(tid.value());
         }
 
         if (manager)
         {
             co_await manager->afterDiscoverTerminus();
+            for (auto& tid : addedTids)
+            {
+                manager->startSensorPolling(tid);
+            }
         }
 
         queuedMctpInfos.pop();
@@ -208,6 +221,11 @@ void TerminusManager::removeMctpTerminus(const MctpInfos& mctpInfos)
         if (it == termini.end())
         {
             continue;
+        }
+
+        if (manager)
+        {
+            manager->stopSensorPolling(it->second->getTid());
         }
 
         unmapTid(it->first);
