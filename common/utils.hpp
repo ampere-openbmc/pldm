e2c34e08efe89b9ffee9a1b387fb096c8dc3156d
Thu Nguyen <thu@os.amperecomputing.com>
2023-03-19 00:38:15 +0700

    platform-mc: discovery: SetDateTime to terminus
    Some termini does not have the date timestamp. It need to be set by
    BMC to start triggering the event.
    
    Change-Id: Ie1ee853343fd335310c32c15161c80700968948f
    Signed-off-by: Thu Nguyen <thu@os.amperecomputing.com>

--- common/utils.hpp
+++ common/utils.hpp
@@ -610,5 +610,18 @@ std::optional<std::string> fruFieldValuestring(const uint8_t* value,
 std::optional<uint32_t> fruFieldParserU32(const uint8_t* value,
                                           const uint8_t& length);
 
+/** @brief Convert epoch time to BCD time
+ *
+ *  @param[in] timeSec - Time got from epoch time in seconds
+ *  @param[out] seconds - number of seconds in BCD
+ *  @param[out] minutes - number of minutes in BCD
+ *  @param[out] hours - number of hours in BCD
+ *  @param[out] day - day of the month in BCD
+ *  @param[out] month - month number in BCD
+ *  @param[out] year - year number in BCD
+ */
+void epochToBCDTime(uint64_t timeSec, uint8_t& seconds, uint8_t& minutes,
+                    uint8_t& hours, uint8_t& day, uint8_t& month,
+                    uint16_t& year);
 } // namespace utils
 } // namespace pldm
