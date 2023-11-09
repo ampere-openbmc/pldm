267e1557e892cbb6780655f2c6db4e3a18b98631
Thu Nguyen <thu@os.amperecomputing.com>
2023-11-09 17:28:09 +0700

    ampere: platform-mc: Support numeric effecter
    This commit supports adding the numeric effecter PDRs to sensor D-Bus
    object path. This allows the value of numeric effecters can be update
    and can setable.
    
    Tested:
    1. Check the sensor D-Bus object path of the numeric effecter PDRs.
    
    Change-Id: I754d0698000fe975cbab6b9e2d8b5a1c474146e7
    Signed-off-by: Thu Nguyen <thu@os.amperecomputing.com>

--- platform-mc/terminus.hpp
+++ platform-mc/terminus.hpp
@@ -290,6 +290,21 @@ class Terminus
      */
     void addNextSensorFromPDRs();
 
+    /** @brief Contruct the Numeric effecter class for the numeric effecter PDR.
+     *
+     *  @param[in] pdr - the compact numeric sensor PDR info
+     */
+    std::shared_ptr<pldm_numeric_effecter_value_pdr>
+        parseNumericEffecterPDR(const std::vector<uint8_t>& Pdr);
+
+    /** @brief Contruct the NumericSensor sensor class for the numeric PLDM
+     *         effecter.
+     *
+     *  @param[in] pdr - the compact numeric sensor PDR info
+     */
+    void addNumericEffecter(
+        const std::shared_ptr<pldm_numeric_effecter_value_pdr> pdr);
+
     /* @brief The terminus's TID */
     pldm_tid_t tid;
 
@@ -341,6 +356,10 @@ class Terminus
     /** @brief Compact Numeric Sensor PDR list */
     std::vector<std::shared_ptr<pldm_compact_numeric_sensor_pdr>>
         compactNumericSensorPdrs{};
+
+    /** @brief Numeric Effecter PDR list */
+    std::vector<std::shared_ptr<pldm_numeric_effecter_value_pdr>>
+        numericEffecterPdrs{};
 
     /** @brief Iteration to loop through sensor PDRs when adding sensors */
     SensorId sensorPdrIt = 0;
