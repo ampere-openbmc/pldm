bfb9274a66f51b1436ed253e27ad412afcf31a61
Thu Nguyen <thu@os.amperecomputing.com>
2023-11-09 17:28:09 +0700

    ampere: platform-mc: Support numeric effecter
    This commit supports adding the numeric effecter PDRs to sensor D-Bus
    object path. This allows the value of numeric effecters can be update
    and can setable.
    
    Tested:
    1. Check the sensor D-Bus object path of the numeric effecter PDRs.
    
    Signed-off-by: Thu Nguyen <thu@os.amperecomputing.com>

--- platform-mc/numeric_sensor.hpp
+++ platform-mc/numeric_sensor.hpp
@@ -63,7 +63,11 @@ class NumericSensor
                   std::shared_ptr<pldm_compact_numeric_sensor_pdr> pdr,
                   std::string& sensorName, std::string& associationPath);
 
-    ~NumericSensor() {};
+    NumericSensor(const pldm_tid_t tid, const bool effecterDisabled,
+                  std::shared_ptr<pldm_numeric_effecter_value_pdr> pdr,
+                  std::string& effecterName, std::string& associationPath);
+
+    ~NumericSensor(){};
 
     /** @brief The function called by Sensor Manager to set sensor to
      * error status.
@@ -211,6 +215,9 @@ class NumericSensor
 
     /** @brief Sensor Unit */
     SensorUnit sensorUnit;
+
+    /** @brief Sensor is effecter */
+    bool isEffecter = false;
 
   private:
     /**
--- platform-mc/terminus.hpp
+++ platform-mc/terminus.hpp
@@ -282,6 +282,21 @@ class Terminus
      */
     void addNextSensorFromPDRs();
 
+    /** @brief Contruct the Numeric effecter class for the numeric effecter PDR.
+     *
+     *  @param[in] pdr - the compact numeric sensor PDR info
+     */
+    std::shared_ptr<pldm_numeric_effecter_value_pdr>
+        parseNumericEffecterPDR(const std::vector<uint8_t>& Pdr);
+
+    /** @brief Contruct the NumericSensor sensor class for the numeric PLDM
+     *         effecter.
+     *
+     *  @param[in] pdr - the compact numeric sensor PDR info
+     */
+    void addNumericEffecter(
+        const std::shared_ptr<pldm_numeric_effecter_value_pdr> pdr);
+
     /* @brief The terminus's TID */
     pldm_tid_t tid;
 
@@ -329,6 +344,10 @@ class Terminus
     std::vector<std::shared_ptr<pldm_compact_numeric_sensor_pdr>>
         compactNumericSensorPdrs{};
 
+    /** @brief Numeric Effecter PDR list */
+    std::vector<std::shared_ptr<pldm_numeric_effecter_value_pdr>>
+        numericEffecterPdrs{};
+    
     /** @brief Iteration to loop through sensor PDRs when adding sensors */
     uint32_t sensorPdrIt = 0;
 };
