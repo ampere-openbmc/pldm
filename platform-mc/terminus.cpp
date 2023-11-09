6fe1cc48b1218b1267380e6386f6ba2085a4becc
Thu Nguyen <thu@os.amperecomputing.com>
2023-11-09 17:28:09 +0700

    ampere: platform-mc: Support numeric effecter
    This commit supports adding the numeric effecter PDRs to sensor D-Bus
    object path. This allows the value of numeric effecters can be update
    and can setable.
    
    Tested:
    1. Check the sensor D-Bus object path of the numeric effecter PDRs.
    
    Signed-off-by: Thu Nguyen <thu@os.amperecomputing.com>

--- platform-mc/terminus.cpp
+++ platform-mc/terminus.cpp
@@ -117,6 +117,8 @@ void Terminus::parseTerminusPDRs()
         numericSensorPdrs{};
     std::vector<std::shared_ptr<pldm_compact_numeric_sensor_pdr>>
         compactNumericSensorPdrs{};
+    std::vector<std::shared_ptr<pldm_numeric_effecter_value_pdr>>
+        numericEffecterPdrs{};
 
     for (auto& pdr : pdrs)
     {
@@ -124,6 +126,7 @@ void Terminus::parseTerminusPDRs()
         switch (pdrHdr->type)
         {
             case PLDM_SENSOR_AUXILIARY_NAMES_PDR:
+            case PLDM_EFFECTER_AUXILIARY_NAMES_PDR:
             {
                 auto sensorAuxNames = parseSensorAuxiliaryNamesPDR(pdr);
                 if (!sensorAuxNames)
@@ -189,6 +192,20 @@ void Terminus::parseTerminusPDRs()
                 entityAuxiliaryNamesTbl.emplace_back(std::move(entityNames));
                 break;
             }
+            case PLDM_NUMERIC_EFFECTER_PDR:
+            {
+                auto parsedPdr = parseNumericEffecterPDR(pdr);
+                if (!parsedPdr)
+                {
+                    lg2::error(
+                        "Failed to parse PDR with type {TYPE} handle {HANDLE}",
+                        "TYPE", pdrHdr->type, "HANDLE",
+                        static_cast<uint32_t>(pdrHdr->record_handle));
+                    continue;
+                }
+                numericEffecterPdrs.emplace_back(std::move(parsedPdr));
+                break;
+            }
             default:
             {
                 lg2::error("Unsupported PDR with type {TYPE} handle {HANDLE}",
@@ -229,6 +246,11 @@ void Terminus::parseTerminusPDRs()
     for (auto pdr : compactNumericSensorPdrs)
     {
         addCompactNumericSensor(pdr);
+    }
+
+    for (auto pdr : numericEffecterPdrs)
+    {
+        addNumericEffecter(pdr);
     }
 }
 
@@ -540,5 +562,68 @@ void Terminus::addCompactNumericSensor(
     }
 }
 
+std::shared_ptr<pldm_numeric_effecter_value_pdr>
+    Terminus::parseNumericEffecterPDR(const std::vector<uint8_t>& pdr)
+{
+    const uint8_t* ptr = pdr.data();
+    auto parsedPdr = std::make_shared<pldm_numeric_effecter_value_pdr>();
+    auto rc = decode_numeric_effecter_pdr_data(ptr, pdr.size(),
+                                               parsedPdr.get());
+    if (rc)
+    {
+        lg2::error("Failed to decode Numeric effecter PDR date, error {RC} ",
+                   "RC", rc);
+        return nullptr;
+    }
+    return parsedPdr;
+}
+
+void Terminus::addNumericEffecter(
+    const std::shared_ptr<pldm_numeric_effecter_value_pdr> pdr)
+{
+    uint16_t sensorId = pdr->effecter_id;
+    if (terminusName == "")
+    {
+        lg2::error(
+            "Terminus ID {TID}: DOES NOT have name. Skip Adding effecters.",
+            "TID", tid);
+        return;
+    }
+    std::string effecterName = terminusName + "_" + "Effecter_" +
+                               std::to_string(sensorId);
+
+    auto sensorAuxiliaryNames = getSensorAuxiliaryNames(sensorId);
+    if (sensorAuxiliaryNames)
+    {
+        const auto& [sensorId, sensorCnt,
+                     effecterNames] = *sensorAuxiliaryNames;
+        if (sensorCnt == 1)
+        {
+            for (const auto& [languageTag, name] : effecterNames[0])
+            {
+                if (languageTag == "en" && name != "")
+                {
+                    effecterName = terminusName + "_" + name;
+                }
+            }
+        }
+    }
+
+    try
+    {
+        auto sensor = std::make_shared<NumericSensor>(
+            tid, true, pdr, effecterName, inventoryPath);
+        lg2::info("Created NumericEffecter {NAME}", "NAME", effecterName);
+        numericSensors.emplace_back(sensor);
+    }
+    catch (const std::exception& e)
+    {
+        lg2::error(
+            "Failed to create NumericEffecter. error - {ERROR} effecterName - {NAME}",
+            "ERROR", e, "NAME", effecterName);
+    }
+}
+
 } // namespace platform_mc
+
 } // namespace pldm
