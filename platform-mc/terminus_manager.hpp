9100acce561bac4d3f8547b9576d9dd89182c22a
Chau Ly <chaul@amperecomputing.com>
2024-03-19 12:33:08 +0000

    platform-mc: Handle Connectivity propertiesChanged signal
    mctpd supports `.Connectivity` property under
    au.com.CodeConstruct.MCTP.Endpoint interface of the endpoint. This
    commit handles the propertiesChanged signal from this interface, and
    updates the Availability of the MCTP Endpoint accordingly in the source
    to enable or disable message sending/receiving via that endpoint of the
    terminus.
    
    When the discovery process first starts, it will only handle the
    endpoints that have `Available` `.Connectivity`. It lets the
    propertiesChanged signal trigger the adding of the endpoints when they
    are back to Available.
    
    On interfaceAdded signal, it assumes that mctpd only publishes available
    endpoints to D-Bus, so it adds the endpoints to the terminus anyway.
    
    Tested:
    Enable unsafe-writable-connectivity option in PACKAGECONFIG of mctp
    recipe. After PLDM discovers all the endpoints, write "Degraded" to
    .Connectivity of one of the endpoint, and write it back to "Available"
    to see how message is stopped from being sent/received via the endpoint.

--- platform-mc/terminus_manager.hpp
+++ platform-mc/terminus_manager.hpp
@@ -146,6 +146,15 @@ class TerminusManager
      */
     bool unmapTid(const pldm_tid_t& tid);
 
+    /** @brief Helper function to invoke registered handlers for
+     *  updating the availability status of the MCTP endpoint
+     *
+     *  @param[in] mctpInfo - information of the target endpoint
+     *  @param[in] availability - new availability status
+     */
+    void updateMctpEndpointAvailability(const MctpInfo& mctpInfo,
+                                        Availability availability);
+
     /** @brief getter of local EID
      *
      *  @return uint8_t - local EID
@@ -274,6 +283,7 @@ class TerminusManager
 
     /** @brief Store the supported MCTP interface info of specific TID */
     std::map<pldm_tid_t, MctpInfos> mctpInfoTable;
+    std::map<MctpInfo, Availability> mctpInfoAvailTable;
 
     /** @brief A queue of MctpInfos to be discovered **/
     std::queue<MctpInfos> queuedMctpInfos{};
