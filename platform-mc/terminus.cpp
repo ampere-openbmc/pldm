5d457cebb0b7d78cb563cda0f7e4dcb27b2e0f64
Thu Nguyen <thu@os.amperecomputing.com>
2024-06-15 02:44:35 +0000

    platform-mc: PDR: Support Entity Aux Name PDRs
    Section `Entity Auxiliary Names PDR` in DSP0248 details about the PDRs
    response for the name of one PLDM entity. When the containerID is
    `0x0000` this entity is `Overall system` or `top most containing
    entity`. The name of this entity will be used as `Terminus name`.
    
    Support parsing `Entity Auxiliary Names PDR` and find the `Terminus
    name` if it is reponsed in the terminus' PDRs.
    
    Signed-off-by: Thu Nguyen <thu@os.amperecomputing.com>

--- platform-mc/terminus.cpp
+++ platform-mc/terminus.cpp
@@ -49,6 +49,30 @@ bool Terminus::doesSupportCommand(uint8_t type, uint8_t command)
     return false;
 }
 
+std::string Terminus::findTerminusName()
+{
+    for (auto entityAuxiliaryNames : entityAuxiliaryNamesTbl)
+    {
+        const auto& [key, entityNames] = *entityAuxiliaryNames;
+        /**
+         * + containerId : 0x0000 = "SYSTEM". If this value is 0x0000, the
+         * containing entity is considered to be the overall system.
+         * + Only have one overall system entity in terminus
+         */
+        if (key.containerId == PLDM_OVERALL_SYTEM_CONTAINER_ID &&
+            key.instanceIdx == 1)
+        {
+            if (entityNames.size() == 0)
+            {
+                return "";
+            }
+            return entityNames[0].second;
+        }
+    }
+
+    return "";
+}
+
 bool Terminus::parsePDRs()
 {
     bool rc = true;
@@ -65,6 +89,11 @@ bool Terminus::parsePDRs()
             auto sensorAuxiliaryNames = parseSensorAuxiliaryNamesPDR(pdr);
             sensorAuxiliaryNamesTbl.emplace_back(
                 std::move(sensorAuxiliaryNames));
+        }
+        else if (pdrHdr->type == PLDM_ENTITY_AUXILIARY_NAMES_PDR)
+        {
+            auto entityNames = parseEntityAuxiliaryNamesPDR(pdr);
+            entityAuxiliaryNamesTbl.emplace_back(std::move(entityNames));
         }
         else if (pdrHdr->type == PLDM_NUMERIC_SENSOR_PDR)
         {
@@ -95,6 +124,13 @@ bool Terminus::parsePDRs()
             rc = false;
         }
     }
+
+    auto tName = findTerminusName();
+    if (tName != "")
+    {
+        terminusName = tName;
+    }
+
     return rc;
 }
 
@@ -159,6 +195,49 @@ std::shared_ptr<SensorAuxiliaryNames>
     }
     return std::make_shared<SensorAuxiliaryNames>(
         pdr->sensor_id, pdr->sensor_count, sensorAuxNames);
+}
+
+std::shared_ptr<EntityAuxiliaryNames>
+    Terminus::parseEntityAuxiliaryNamesPDR(const std::vector<uint8_t>& pdrData)
+{
+    constexpr uint8_t nullTerminator = 0;
+    auto pdr = reinterpret_cast<const struct pldm_entity_auxiliary_names_pdr*>(
+        pdrData.data());
+    const uint8_t* ptr = pdr->names;
+    char16_t alignedBuffer[PLDM_STR_UTF_16_MAX_LEN];
+    const uint8_t nameStringCount = static_cast<uint8_t>(*ptr);
+    ptr += sizeof(uint8_t);
+    std::vector<std::pair<NameLanguageTag, EntityName>> nameStrings{};
+    for (int j = 0; j < nameStringCount; j++)
+    {
+        std::string nameLanguageTag(reinterpret_cast<const char*>(ptr), 0,
+                                    PLDM_STR_UTF_8_MAX_LEN);
+        ptr += nameLanguageTag.size() + sizeof(nullTerminator);
+
+        int u16NameStringLen = 0;
+        for (int i = 0; ptr[i] != 0 || ptr[i + 1] != 0; i += 2)
+        {
+            u16NameStringLen++;
+        }
+        memset(alignedBuffer, 0, PLDM_STR_UTF_16_MAX_LEN * sizeof(uint16_t));
+        memcpy(alignedBuffer, ptr, u16NameStringLen * sizeof(uint16_t));
+        std::u16string u16NameString(alignedBuffer, 0, PLDM_STR_UTF_16_MAX_LEN);
+        ptr += (u16NameString.size() + sizeof(nullTerminator)) *
+               sizeof(uint16_t);
+        std::transform(u16NameString.cbegin(), u16NameString.cend(),
+                       u16NameString.begin(),
+                       [](uint16_t utf16) { return be16toh(utf16); });
+        std::string nameString =
+            std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t>{}
+                .to_bytes(u16NameString);
+        nameStrings.emplace_back(std::make_pair(nameLanguageTag, nameString));
+    }
+
+    EntityKey key{pdr->container.entity_type,
+                  pdr->container.entity_instance_num,
+                  pdr->container.entity_container_id};
+
+    return std::make_shared<EntityAuxiliaryNames>(key, nameStrings);
 }
 
 std::shared_ptr<pldm_numeric_sensor_value_pdr>
